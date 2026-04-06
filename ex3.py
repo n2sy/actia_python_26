d18205ebef8a2f73946b90c7f8da97b26250adc9
n2sy <nidhal.jelassi@fsegt.utm.tn>
2026-04-06 14:25:03 +0100

    exercice 3

--- ex3.py
+++ ex3.py
@@ -1,9 +1,19 @@
-prenom = "nidhal"
-age = 105
-# d = f"Je m'appelle {prenom} et j'ai {age} ans !"
+# prenom = "nidhal"
+# nom = "jelassi"
 
-# print(d)
+# s = "Je m'appelle {} -- {}".format(prenom, nom)
+# print(s)
+prix_HT = input("Donnez le prix : ")
+montant = input("Entrez le montant dont vous disposez : ")
 
-if age < 60:
-    print("jeune retraité")
-print("Fin")
+prix_TTC = int(prix_HT) * 1.19
+print(prix_TTC)
+
+if float(montant) < prix_TTC:
+    print(
+        f"Vous ne pouvez pas vous l'offrir, il vous manque {(prix_TTC - int(montant)):.2f} dinars"
+    )
+else:
+    print(
+        f"Vous pouvez vous l'offrir, il vous reste {(int(montant) - prix_TTC):.2f} dinars"
+    )
