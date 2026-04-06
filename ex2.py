00000d03ea6a67c67f2589288871f5c972b9603e
n2sy <nidhal.jelassi@fsegt.utm.tn>
2026-04-06 13:53:22 +0100

    exercice 2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+a = "J'ai une classe de " + str(30) + " élèves"
+b = str(10) + " + " + "5" + " est égal à " + str(15)
+c = str(10) + "5"
+d = "L'addition de 10 + 5 est égal à " + str(10 + 5)
+
+print(a)
+print(b)
+print(c)
+print(d)
---
+++ ex3.py
@@ -0,0 +1,9 @@
+prenom = "nidhal"
+age = 105
+# d = f"Je m'appelle {prenom} et j'ai {age} ans !"
+
+# print(d)
+
+if age < 60:
+    print("jeune retraité")
+print("Fin")
